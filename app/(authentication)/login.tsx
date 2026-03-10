283f759c94084dfc876069a671626c9a38fbc9fa
Angel Hernandez Barcenas <angelhb015@gmail.com>
2026-03-10 17:21:45 -0400

    Integrated supabase for user authentication

--- app/(authentication)/login.tsx
+++ app/(authentication)/login.tsx
@@ -1,14 +1,13 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import Auth from '@/components/Auth';
 
 const Login = () => {
   return (
     <SafeAreaView className='h-full'>
-      <View className='flex bg-blue-500 m-auto w-[70%] h-[50%]'>
-        <Text className='text-4xl'>Login</Text>
-        <TextInput className='text-2xl' placeholder='hello' />
-      </View>
+      <Auth />
+
     </SafeAreaView>
   )
 }
