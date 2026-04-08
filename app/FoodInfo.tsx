230ec3150d6626aa9858868ba55c3323a61164aa
Angel Hernandez Barcenas <angelhb015@gmail.com>
2026-04-07 21:48:11 -0400

    Organized the food list

---
+++ app/FoodInfo.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const foodInfo = () => {
+  return (
+    <View>
+      <Text>foodInfo</Text>
+    </View>
+  )
+}
+
+export default foodInfo
+
+const styles = StyleSheet.create({})
