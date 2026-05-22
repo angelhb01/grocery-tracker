b48a2e415c1eafc9c4186e13de9a7d8423f7c74d
Angel Hernandez Barcenas <angelhb015@gmail.com>
2026-05-21 22:44:15 -0400

    Fixed scrollview in AddFood.tsx

--- app/(screens)/addFood.tsx
+++ app/(screens)/addFood.tsx
@@ -108,212 +108,211 @@ const AddFoodScreen = () => {
 
   return (
     <KeyboardAvoidingView
+      className="flex-1"
       behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <SafeAreaView className="">
-          <ScrollView contentContainerClassName="flex-col justify-center mt-5 mx-5 gap-7 pb-5">
-            <View className="flex-col justify-center w-full items-center">
-              <View className="flex-row w-full">
-                <Button
-                  className="rounded-full absolute bg-green-100 w-[3rem] h-[3rem] flex justify-center items-center p-0"
-                  onPress={() => router.back()}
-                >
-                  <Ionicons
-                    name="arrow-back-outline"
-                    size={20}
-                    color="#008000"
-                  />
-                </Button>
-                <View className="w-[4.5rem] h-[4.5rem] rounded-full bg-green-100 m-auto flex justify-center items-center">
-                  <FontAwesome
-                    name="shopping-basket"
-                    size={24}
-                    color="#008000"
-                  />
-                </View>
-              </View>
-              <Text className="text-[2rem] text-[#008000]">Add Food Item</Text>
-              <Text className="text-xs text-slate-500">
-                Add a new item to your groceries
-              </Text>
-            </View>
-            <View
-              className="bg-white p-10 flex-col gap-5 rounded-xl"
-              style={styles.formContainer}
-            >
-              {/* Add product description */}
-              <View className="flex-col gap-1">
-                <Text>
-                  Name:
-                  <Text className="text-red-500">*</Text>
-                </Text>
-                <TextInput
-                  className="bg-white text-black h-12 p-2 border-[1px] rounded-md"
-                  value={productName}
-                  onChangeText={(text) => setProductName(text)}
-                  autoCapitalize="none"
-                />
-              </View>
-              <View className="flex-col gap-1">
-                <Text>Description:</Text>
-                <TextInput
-                  className="bg-white text-black h-12 p-2 border-[1px] rounded-md"
-                  value={productDescription}
-                  onChangeText={(text) => setProductDescription(text)}
-                  autoCapitalize="none"
-                />
-              </View>
-              <View className="flex-col gap-1">
-                <Text>
-                  Type:
-                  <Text className="text-red-500">*</Text>
-                </Text>
-                <DropDownPicker
-                  style={{ backgroundColor: "white" }}
-                  dropDownContainerStyle={{
-                    backgroundColor: "white",
-                    zIndex: 1000,
-                  }}
-                  open={open}
-                  value={productType}
-                  items={items}
-                  setOpen={setOpen}
-                  setValue={setProductType}
-                  setItems={setItems}
-                />
-              </View>
-              {/* Numerical inputs */}
-              <View className="gap-2 flex-col">
-                <View className="flex flex-row justify-start gap-10">
-                  <Text className="min-w-[8rem]">
-                    Calories:
-                    <Text className="text-red-500">*</Text>
-                  </Text>
-                  <View className="bg-white border-[1px] rounded-md ml-auto h-[3rem] flex-row justify-start flex-1">
-                    <View className="h-full w-[2.5rem] border-r-[1px] items-center justify-center">
-                      <SimpleLineIcons name="fire" size={24} color="#008000" />
-                    </View>
-                    <TextInput
-                      className=" text-black p-1 h-full flex-1"
-                      keyboardType="decimal-pad"
-                      value={calories}
-                      onChangeText={(text) =>
-                        setCalories(handleTextChange(text))
-                      }
-                      autoCapitalize="none"
-                    />
-                  </View>
-                </View>
-                <View className="flex flex-row justify-start gap-10">
-                  <Text className="min-w-[8rem]">
-                    Fat (g):
-                    <Text className="text-red-500">*</Text>
-                  </Text>
-                  <View className="bg-white border-[1px] rounded-md ml-auto h-[3rem] flex-row justify-start flex-1">
-                    <View className="h-full w-[2.5rem] border-r-[1px] items-center justify-center">
-                      <Ionicons
-                        name="water-outline"
-                        size={24}
-                        color="#008000"
-                      />
-                    </View>
-                    <TextInput
-                      className="text-black p-1 h-full flex-1"
-                      keyboardType="decimal-pad"
-                      value={fat}
-                      onChangeText={(text) => setFat(handleTextChange(text))}
-                      autoCapitalize="none"
-                    />
-                  </View>
-                </View>
-                <View className="flex flex-row justify-between gap-10">
-                  <Text className="min-w-[8rem]">
-                    Carbs (g):
-                    <Text className="text-red-500">*</Text>
-                  </Text>
-                  <View className="bg-white border-[1px] rounded-md ml-auto h-[3rem] flex-row justify-start flex-1">
-                    <View className="h-full w-[2.5rem] border-r-[1px] items-center justify-center">
-                      <FontAwesome6
-                        name="jar-wheat"
-                        size={24}
-                        color="#008000"
-                      />
-                    </View>
-                    <TextInput
-                      className="text-black p-1 h-full flex-1"
-                      keyboardType="decimal-pad"
-                      value={carbs}
-                      onChangeText={(text) => setCarbs(handleTextChange(text))}
-                      autoCapitalize="none"
-                    />
-                  </View>
-                </View>
-                <View className="flex flex-row justify-between gap-10">
-                  <Text className="min-w-[8rem]">
-                    Protein (g):
-                    <Text className="text-red-500">*</Text>
-                  </Text>
-                  <View className="bg-white border-[1px] rounded-md ml-auto h-[3rem] flex-row justify-start flex-1">
-                    <View className="h-full w-[2.5rem] border-r-[1px] items-center justify-center">
-                      <MaterialCommunityIcons
-                        name="arm-flex-outline"
-                        size={24}
-                        color="#008000"
-                      />
-                    </View>
-                    <TextInput
-                      className="text-black p-1 h-full flex-1"
-                      keyboardType="decimal-pad"
-                      value={protein}
-                      onChangeText={(text) =>
-                        setProtein(handleTextChange(text))
-                      }
-                      autoCapitalize="none"
-                    />
-                  </View>
-                </View>
-                <View className="flex flex-row justify-between gap-10">
-                  <Text className="min-w-[8rem]">
-                    Quantity:
-                    <Text className="text-red-500">*</Text>
-                  </Text>
-                  <View className="bg-white border-[1px] rounded-md ml-auto h-[3rem] flex-row justify-start flex-1">
-                    <View className="h-full w-[2.5rem] border-r-[1px] flex justify-center items-center">
-                      <Feather name="shopping-bag" size={25} color="#008000" />
-                    </View>
-                    <TextInput
-                      className="text-black p-1 h-full flex-1"
-                      keyboardType="decimal-pad"
-                      value={quantity}
-                      onChangeText={(text) =>
-                        setQuantity(handleTextChange(text))
-                      }
-                      autoCapitalize="none"
-                    />
-                  </View>
-                </View>
-              </View>
-              {/* Button to add to groceries */}
+      <SafeAreaView className="flex-1">
+        <ScrollView contentContainerClassName="flex-col justify-center mt-5 mx-5 gap-7 pb-5 flex-grow">
+          <View className="flex-col justify-center w-full items-center">
+            <View className="flex-row w-full">
               <Button
-                className="flex gap-5 bg-green-600"
-                onPress={addToGroceries}
-                disabled={loading}
+                className="rounded-full absolute bg-green-100 w-[3rem] h-[3rem] flex justify-center items-center p-0"
+                onPress={() => router.back()}
               >
-                <View>
-                  <FontAwesome6 name="add" size={24} color="white" />
-                </View>
-                <View>
-                  <ButtonText className="text-white">
-                    Add to Groceries
-                  </ButtonText>
-                </View>
+                <Ionicons
+                  name="arrow-back-outline"
+                  size={20}
+                  color="#008000"
+                />
               </Button>
+              <View className="w-[4.5rem] h-[4.5rem] rounded-full bg-green-100 m-auto flex justify-center items-center">
+                <FontAwesome
+                  name="shopping-basket"
+                  size={24}
+                  color="#008000"
+                />
+              </View>
             </View>
-          </ScrollView>
-        </SafeAreaView>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+            <Text className="text-[2rem] text-[#008000]">Add Food Item</Text>
+            <Text className="text-xs text-slate-500">
+              Add a new item to your groceries
+            </Text>
+          </View>
+          <View
+            className="bg-white p-10 flex-col gap-5 rounded-xl"
+            style={styles.formContainer}
+          >
+            {/* Add product description */}
+            <View className="flex-col gap-1">
+              <Text>
+                Name:
+                <Text className="text-red-500">*</Text>
+              </Text>
+              <TextInput
+                className="bg-white text-black h-12 p-2 border-[1px] rounded-md"
+                value={productName}
+                onChangeText={(text) => setProductName(text)}
+                autoCapitalize="none"
+              />
+            </View>
+            <View className="flex-col gap-1">
+              <Text>Description:</Text>
+              <TextInput
+                className="bg-white text-black h-12 p-2 border-[1px] rounded-md"
+                value={productDescription}
+                onChangeText={(text) => setProductDescription(text)}
+                autoCapitalize="none"
+              />
+            </View>
+            <View className="flex-col gap-1">
+              <Text>
+                Type:
+                <Text className="text-red-500">*</Text>
+              </Text>
+              <DropDownPicker
+                style={{ backgroundColor: "white" }}
+                dropDownContainerStyle={{
+                  backgroundColor: "white",
+                  zIndex: 1000,
+                }}
+                open={open}
+                value={productType}
+                items={items}
+                setOpen={setOpen}
+                setValue={setProductType}
+                setItems={setItems}
+              />
+            </View>
+            {/* Numerical inputs */}
+            <View className="gap-2 flex-col">
+              <View className="flex-row items-center gap-10 w-full">
+                <Text className="min-w-[40%]">
+                  Calories:
+                  <Text className="text-red-500">*</Text>
+                </Text>
+                <View className="bg-white border-[1px] rounded-md h-[3rem] flex-row flex-1">
+                  <View className="h-full w-[2.5rem] border-r-[1px] items-center justify-center">
+                    <SimpleLineIcons name="fire" size={24} color="#008000" />
+                  </View>
+                  <TextInput
+                    className="text-black p-1 h-full flex-1"
+                    keyboardType="decimal-pad"
+                    value={calories}
+                    onChangeText={(text) =>
+                      setCalories(handleTextChange(text))
+                    }
+                    autoCapitalize="none"
+                  />
+                </View>
+              </View>
+              <View className="flex-row items-center gap-10">
+                <Text className="min-w-[40%]">
+                  Fat (g):
+                  <Text className="text-red-500">*</Text>
+                </Text>
+                <View className="bg-white border-[1px] rounded-md ml-auto h-[3rem] flex-row justify-start flex-1">
+                  <View className="h-full w-[2.5rem] border-r-[1px] items-center justify-center">
+                    <Ionicons
+                      name="water-outline"
+                      size={24}
+                      color="#008000"
+                    />
+                  </View>
+                  <TextInput
+                    className="text-black p-1 h-full flex-1"
+                    keyboardType="decimal-pad"
+                    value={fat}
+                    onChangeText={(text) => setFat(handleTextChange(text))}
+                    autoCapitalize="none"
+                  />
+                </View>
+              </View>
+              <View className="flex-row items-center gap-10">
+                <Text className="min-w-[40%]">
+                  Carbs (g):
+                  <Text className="text-red-500">*</Text>
+                </Text>
+                <View className="bg-white border-[1px] rounded-md ml-auto h-[3rem] flex-row justify-start flex-1">
+                  <View className="h-full w-[2.5rem] border-r-[1px] items-center justify-center">
+                    <FontAwesome6
+                      name="jar-wheat"
+                      size={24}
+                      color="#008000"
+                    />
+                  </View>
+                  <TextInput
+                    className="text-black p-1 h-full flex-1"
+                    keyboardType="decimal-pad"
+                    value={carbs}
+                    onChangeText={(text) => setCarbs(handleTextChange(text))}
+                    autoCapitalize="none"
+                  />
+                </View>
+              </View>
+              <View className="flex-row items-center gap-10">
+                <Text className="min-w-[40%]">
+                  Protein (g):
+                  <Text className="text-red-500">*</Text>
+                </Text>
+                <View className="bg-white border-[1px] rounded-md ml-auto h-[3rem] flex-row justify-start flex-1">
+                  <View className="h-full w-[2.5rem] border-r-[1px] items-center justify-center">
+                    <MaterialCommunityIcons
+                      name="arm-flex-outline"
+                      size={24}
+                      color="#008000"
+                    />
+                  </View>
+                  <TextInput
+                    className="text-black p-1 h-full flex-1"
+                    keyboardType="decimal-pad"
+                    value={protein}
+                    onChangeText={(text) =>
+                      setProtein(handleTextChange(text))
+                    }
+                    autoCapitalize="none"
+                  />
+                </View>
+              </View>
+              <View className="flex-row items-center gap-10">
+                <Text className="min-w-[40%]">
+                  Quantity:
+                  <Text className="text-red-500">*</Text>
+                </Text>
+                <View className="bg-white border-[1px] rounded-md ml-auto h-[3rem] flex-row justify-start flex-1">
+                  <View className="h-full w-[2.5rem] border-r-[1px] flex justify-center items-center">
+                    <Feather name="shopping-bag" size={25} color="#008000" />
+                  </View>
+                  <TextInput
+                    className="text-black p-1 h-full flex-1"
+                    keyboardType="decimal-pad"
+                    value={quantity}
+                    onChangeText={(text) =>
+                      setQuantity(handleTextChange(text))
+                    }
+                    autoCapitalize="none"
+                  />
+                </View>
+              </View>
+            </View>
+            {/* Button to add to groceries */}
+            <Button
+              className="flex gap-5 bg-green-600"
+              onPress={addToGroceries}
+              disabled={loading}
+            >
+              <View>
+                <FontAwesome6 name="add" size={24} color="white" />
+              </View>
+              <View>
+                <ButtonText className="text-white">
+                  Add to Groceries
+                </ButtonText>
+              </View>
+            </Button>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </KeyboardAvoidingView >
   );
 };
 
---
+++ components/FoodItem.tsx
@@ -0,0 +1,5 @@
+const FoodItem = () => {
+
+}
+
+export default FoodItem
