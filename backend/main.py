134daca8e9b5601522036c6c58c407b765f89582
Angel Hernandez Barcenas <angelhb015@gmail.com>
2026-06-07 21:13:50 -0400

    Added YOLO model to detect objects

--- backend/main.py
+++ backend/main.py
@@ -1,44 +1,39 @@
-"""
+from ultralytics import YOLO
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile
-from transformers import pipeline
 from PIL import Image
 import io
 
-
 # load the model
-classifier = pipeline("image-classification", model="nateraw/food")
+model = YOLO("yolo26s.pt")
 
-app = FastAPI()
-
-# Allowed origins
-origins = [
-    "http://localhost:8081"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Endpoints
-
-@app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-    # Read image bytes
-    image_bytes = await file.read()
-
-    # convert bytes to PIL image
-    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+def main():
+    app = FastAPI()
     
-    # run inference
-    res = classifier(image)
+    # Middleware
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        #allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    
+    # Endpoints
+    @app.post("/predict")
+    async def predict(file: UploadFile = File(...)):
+        # Read image bytes
+        image_bytes = await file.read()
+    
+        # convert bytes to PIL image
+        image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+        
+        # run inference
+        results = model(image)
+    
+        return results[0].to_json()
+    return app
 
-    return {
-        "filename": file.filename,
-        "predictions": res
-    }
-""""
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(main, host="0.0.0.0", port=8000)
